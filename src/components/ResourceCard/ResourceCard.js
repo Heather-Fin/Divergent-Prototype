a5581188ae649cba1b1c2f0211d1d792c18ac2c4
Heather <43322948+Heather-Fin@users.noreply.github.com>
2019-07-29 14:12:56 -0500

    Add chevrons

--- src/components/ResourceCard/ResourceCard.js
+++ src/components/ResourceCard/ResourceCard.js
@@ -4,11 +4,14 @@ import {
     ClickableTile,
 } from 'carbon-components-react/lib/components/Tile';
 
+import ChevronRight20 from '@carbon/icons-react/lib/chevron--right/20';
+
 const ResourceCard = () => (
     
     <ClickableTile className='tile'>
         <h2>Resource Tile</h2>
         <p>Lorem ipsum delores</p>
+        <ChevronRight20 className='arrow'/>
     </ClickableTile>
 );
 
