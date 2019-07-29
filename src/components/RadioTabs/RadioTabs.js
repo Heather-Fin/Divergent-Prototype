6bc80c5a4078035647276b695c40e46a77093585
Heather <43322948+Heather-Fin@users.noreply.github.com>
2019-07-29 15:47:44 -0500

    Redid radio buttons - unstyled

--- src/components/RadioTabs/RadioTabs.js
+++ src/components/RadioTabs/RadioTabs.js
@@ -1,31 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import RadioButtonGroup from 'carbon-components-react/lib/components/RadioButtonGroup';
-import RadioButton from 'carbon-components-react/lib/components/RadioButton';
-import FormGroup from 'carbon-components-react/lib/components/FormGroup';
+// import RadioButtonGroup from 'carbon-components-react/lib/components/RadioButtonGroup';
+// import RadioButton from 'carbon-components-react/lib/components/RadioButton';
+// import FormGroup from 'carbon-components-react/lib/components/FormGroup';
 
-const RadioTab = () => (
-        <FormGroup legendText="Radio Buttons Heading">
-          <RadioButtonGroup
-            defaultSelected="default-selected"
-            legend="Group Legend"
-          >
-            <RadioButton 
-                value="default-selected"
-                checked={true}
-                id="radio-1"
-                labelText='Button 1' />
-            <RadioButton
-                value="standard-2"
-                id="radio-2"
-                labelText='Button 2'
-                />
-            <RadioButton value="standard-3" id="radio-3" labelText='Button 3'/>
-            <RadioButton value="standard-4" id="radio-4" labelText='Button 4'/>
-            <RadioButton value="standard-5" id="radio-5" labelText='Button 5'/>
-            <RadioButton value ='standard-6' id="radio-6" labelText='Button 6'/>
-          </RadioButtonGroup>
-        </FormGroup>
-);
+class RadioTab extends Component {
+    state = {
+        selected: 'radio-1'
+    };
+    componentDidUpdate () {
+        if (this.state.selected === 'radio-1') {
+            console.log("radio-1")
+        } else {
+            console.log("radio-2")
+        }
+    };
+    render () {
+        return (
+            <form>
+                <label>
+                    <input
+                        type="radio"
+                        id='radio-1'
+                        name='myRadio'
+                        value='radio-1'
+                        checked={this.state.selected === 'radio-1'}
+                        onChange={(e) => this.setState({ selected: e.target.value })}
+                    />
+                    Button 1
+                </label>
+                <label>
+                    <input
+                        type="radio"
+                        id='radio-2'
+                        name='myRadio'
+                        value='radio-2'
+                        checked={this.state.selected === 'radio-2'}
+                        onChange={(e) => this.setState({ selected: e.target.value })}
+                    />
+                    Button 2
+                </label>
+            </form>
+        );
+    }
+};
 
 export default RadioTab;
