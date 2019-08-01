9a4d65014ca8e5b0f6d9727556c8905560661da6
Heather <43322948+Heather-Fin@users.noreply.github.com>
2019-08-01 16:58:23 -0500

    Added grid to employee card

--- src/components/EmployeeCard/EmployeeCard.js
+++ src/components/EmployeeCard/EmployeeCard.js
@@ -9,14 +9,26 @@ class EmployeeCard extends Component {
         return (
           <div role="group" aria-label="selectable tiles">
             <SelectableTile id="tile-1" name="tiles" >
-              <h1>{this.props.initials}</h1>
-              <p>{this.props.description}</p>
-              <h2>Location & Band:</h2>
-                <p>{this.props.location}</p>
-                <p>{this.props.bandLevel}</p>
-              <h2>Enhances team in:</h2>
-              <h2>Start Date:</h2>
-              <p>{this.props.startDate}</p>
+                <div className="bx--grid bx--grid--full-width">
+                    <div className="bx--row">
+                        <div className="bx--col">
+                            <h1>{this.props.initials}</h1>
+                            <p>{this.props.description}</p>
+                        </div>
+                        <div className="bx--col">
+                            <h2>Location & Band:</h2>
+                            <p>{this.props.location}</p>
+                            <p>{this.props.bandLevel}</p>
+                        </div>
+                        <div className="bx--col">
+                            <h2>Enhances team in:</h2>
+                        </div>
+                        <div className="bx--col">
+                            <h2>Start Date:</h2>
+                            <p>{this.props.startDate}</p>
+                        </div>
+                    </div>
+                </div>
             </SelectableTile>
           </div>
         );
