99e4a4d195c6f946b8f54b35c324a21a039759fc
Heather <43322948+Heather-Fin@users.noreply.github.com>
2019-08-05 14:21:41 -0500

    Resource cards added to landing page

--- src/content/ComparisonPage/ComparisonPage.js
+++ src/content/ComparisonPage/ComparisonPage.js
@@ -22,50 +22,58 @@ const ComparisonPage = () => {
             <h1 className="comparison-page--h1">Talent Pool Comparison</h1>
         </div>
         <h2 className="comparison-page--h2">— Compare Your Candidates —</h2>
-        <Tile>
-            <table className="comparison-page--tile">
-                <tr className="comparison-page--table-header">
-                    <th><h2 className="comparison-page--h1">AST</h2><p className="comparison-page--left-header comparison-page--p">I've lived in over 30 countries and love meeting new people. I have been self employed while traveling, and am comfortable with negotiating with team members.</p></th>
-                    <th><h2 className="comparison-page--right-header comparison-page--h1">HRU</h2><p className="comparison-page--right-header comparison-page--p">I am from a large city and have experience working in a start-up environment. I work well in agile workspaces and hope to learn more about designing in a corporate workspace.</p></th>
-                </tr>
-                <tr>
-                    <td><CheckMarkFilled20 className="comparison-page--icon"/>Perspective</td>
-                    <td><CheckMarkFilled20 className="comparison-page--icon"/>Perspective</td>
-                </tr>
-                <tr>
-                    <td><CheckMarkOutline20 className="comparison-page--icon"/>Skills</td>
-                    <td><CheckMarkFilled20 className="comparison-page--icon"/>Skills</td>
-                </tr>
-                <tr>
-                    <td><CheckMarkFilled20 className="comparison-page--icon"/>Education</td>
-                    <td><CheckMarkFilled20 className="comparison-page--icon"/>Education</td>
-                </tr>
-                <tr>
-                    <td><CheckMarkFilled20 className="comparison-page--icon"/>Interests</td>
-                    <td><CheckMarkOutline20 className="comparison-page--icon"/>Interests</td>
-                </tr>
-                <tr>
-                    <td><CheckMarkFilled20 className="comparison-page--icon"/>Personality</td>
-                    <td><CheckMarkFilled20 className="comparison-page--icon"/>Personality</td>
-                </tr>
-                <tr>
-                    <td><CheckMarkOutline20 className="comparison-page--icon"/>Languages</td>
-                    <td><CheckMarkOutline20 className="comparison-page--icon"/>Languages</td>
-                </tr>
-                <tr>
-                    <td>
-                        <Button onClick={console.log('clicked meee 1')}>
-                            Pin Candidate<Pin20 className="bx--btn__icon"/>
-                        </Button>
-                    </td>
-                    <td>
-                        <Button onClick={console.log('clicked meee 1')}>
-                            Pin Candidate<Pin20 className="bx--btn__icon"/>
-                        </Button>
-                    </td>
-                </tr>
-            </table>
-        </Tile>
+        <div className="bx--grid bx--grid--full-width">
+            <div className="bx--row">
+                <div className="bx--col-lg-1" />
+                <div className="bx--col-lg-10">
+                    <Tile>
+                        <table className="comparison-page--tile">
+                            <tr className="comparison-page--table-header">
+                                <th><h2 className="comparison-page--h1">AST</h2><p className="comparison-page--left-header comparison-page--p">I've lived in over 30 countries and love meeting new people. I have been self employed while traveling, and am comfortable with negotiating with team members.</p></th>
+                                <th><h2 className="comparison-page--right-header comparison-page--h1">HRU</h2><p className="comparison-page--right-header comparison-page--p">I am from a large city and have experience working in a start-up environment. I work well in agile workspaces and hope to learn more about designing in a corporate workspace.</p></th>
+                            </tr>
+                            <tr>
+                                <td><CheckMarkFilled20 className="comparison-page--icon"/>Perspective</td>
+                                <td><CheckMarkFilled20 className="comparison-page--icon"/>Perspective</td>
+                            </tr>
+                            <tr>
+                                <td><CheckMarkOutline20 className="comparison-page--icon"/>Skills</td>
+                                <td><CheckMarkFilled20 className="comparison-page--icon"/>Skills</td>
+                            </tr>
+                            <tr>
+                                <td><CheckMarkFilled20 className="comparison-page--icon"/>Education</td>
+                                <td><CheckMarkFilled20 className="comparison-page--icon"/>Education</td>
+                            </tr>
+                            <tr>
+                                <td><CheckMarkFilled20 className="comparison-page--icon"/>Interests</td>
+                                <td><CheckMarkOutline20 className="comparison-page--icon"/>Interests</td>
+                            </tr>
+                            <tr>
+                                <td><CheckMarkFilled20 className="comparison-page--icon"/>Personality</td>
+                                <td><CheckMarkFilled20 className="comparison-page--icon"/>Personality</td>
+                            </tr>
+                            <tr>
+                                <td><CheckMarkOutline20 className="comparison-page--icon"/>Languages</td>
+                                <td><CheckMarkOutline20 className="comparison-page--icon"/>Languages</td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <Button onClick={console.log('clicked meee 1')}>
+                                        Pin Candidate<Pin20 className="bx--btn__icon"/>
+                                    </Button>
+                                </td>
+                                <td>
+                                    <Button onClick={console.log('clicked meee 1')}>
+                                        Pin Candidate<Pin20 className="bx--btn__icon"/>
+                                    </Button>
+                                </td>
+                            </tr>
+                        </table>
+                    </Tile>
+                </div>
+                <div className="bx--col-lg-1" />
+            </div>
+        </div>
     </div>
   );
 };
--- src/content/LandingPage/LandingPage.js
+++ src/content/LandingPage/LandingPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import RadarGraphImg from '../../assets/RadarGraph.png';
-//import ResourceCard from '../../components/ResourceCard';
+import ResourceCard from '../../components/ResourceCard';
 
 // icons
 import CheckMarkFilled20 from '@carbon/icons-react/lib/checkmark--filled/20';
@@ -24,20 +24,27 @@ class LandingPage extends Component {
             </div>
             <div className="bx--col-md-2 landing-page--container">
               <h2 className="landing-page--h2">Your Strengths:</h2>
-              
-                <h3 className="landing-page--h3"><CheckMarkFilled20 className="landing-pace--icons"/>Education</h3>
+                <h3 className="landing-page--h3"><CheckMarkFilled20 className="landing-page--icons"/>Education</h3>
                 <p className="landing-page--p">Your team has a wide variety of educational backgrounds and certifications. This means your team is well-equipped to tackle problems from many different academic perspectives.</p>
-                <h3 className="landing-page--h3"><CheckMarkFilled20 className="landing-pace--icons"/>Languages</h3>
+                <h3 className="landing-page--h3"><CheckMarkFilled20 className="landing-page--icons"/>Languages</h3>
                 <p className="landing-page--p">Your team has a diverse set of communication styles, meaning your team has many ways of listening and sharing ideas.</p>
             </div>
             <div className="bx--col-md-2 landing-page--container">
               <h2 className="landing-page--h2">Room to Improve:</h2>
-              <h3 className="landing-page--h3"><ErrorFilled20 className="landing-pace--icons"/>Skills</h3>
+              <h3 className="landing-page--h3"><ErrorFilled20 className="landing-page--icons"/>Skills</h3>
               <p className="landing-page--p">Your team's skills are limited mostly to technical experience. Tap into your team's potential to grow their skillset through the resources below.</p>
-              <h3 className="landing-page--h3"><ErrorFilled20 className="landing-pace--icons"/>Culture</h3>
+              <h3 className="landing-page--h3"><ErrorFilled20 className="landing-page--icons"/>Culture</h3>
               <p className="landing-page--p">Having a low score in culture means your team comes from similar backgrounds. You may have a limited perspective when tackling new projects.</p>
             </div>
           </div>
+        </div>
+        <div className="bx--row">
+          <h1>Suggested Resources</h1>
+          <p>Learn how to empower and diversify your team! These IBM resources were selected based on the areas your team has the most room to improve in.</p>
+          <p>You can access these tools now, or bookmark them to YourLearning for later access.</p>
+          <ResourceCard />
+          <ResourceCard />
+          <ResourceCard />
         </div>
       </div>
     );
