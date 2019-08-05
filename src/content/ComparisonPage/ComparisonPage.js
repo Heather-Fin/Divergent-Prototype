80b9aabe0b79f20930a80331e7ee39a2c8db7baa
Heather <43322948+Heather-Fin@users.noreply.github.com>
2019-08-04 19:11:35 -0500

    modified comparison page

--- src/content/ComparisonPage/ComparisonPage.js
+++ src/content/ComparisonPage/ComparisonPage.js
@@ -8,7 +8,7 @@ import Button from 'carbon-components-react/lib/components/Button';
 
 //icons
 import CheckMarkFilled20 from '@carbon/icons-react/lib/checkmark--filled/20';
-//import CheckMarkOutline20 from '@carbon/icons-react/lib/checkmark--outline/20';
+import CheckMarkOutline20 from '@carbon/icons-react/lib/checkmark--outline/20';
 import Pin20 from '@carbon/icons-react/lib/pin/20';
 import LeftChevron20 from '@carbon/icons-react/lib/chevron--left/20';
 
@@ -25,7 +25,7 @@ const ComparisonPage = () => {
         <Tile>
             <table className="comparison-page--tile">
                 <tr className="comparison-page--table-header">
-                    <th><h1>JKY</h1><p className="comparison-page--left-header">Lorem ipsum dolor sit amet, ex erant legere eam, quot eleifend aliquando ea vim. Erat corpora platonem quo cu. Sensibus delicatissimi eum id, mel quidam equidem ut. Vis elit vitae temporibus.</p></th>
+                    <th><h1>HRU</h1><p className="comparison-page--left-header">Lorem ipsum dolor sit amet, ex erant legere eam, quot eleifend aliquando ea vim. Erat corpora platonem quo cu. Sensibus delicatissimi eum id, mel quidam equidem ut. Vis elit vitae temporibus.</p></th>
                     <th><h1 className="comparison-page--right-header">AST</h1><p className="comparison-page--right-header">Lorem ipsum dolor sit amet, ex erant legere eam, quot eleifend aliquando ea vim. Erat corpora platonem quo cu. Sensibus delicatissimi eum id, mel quidam equidem ut. Vis elit vitae temporibus.</p></th>
                 </tr>
                 <tr>
@@ -34,23 +34,23 @@ const ComparisonPage = () => {
                 </tr>
                 <tr>
                     <td><CheckMarkFilled20 className="comparison-page--icon"/>Skills</td>
-                    <td><CheckMarkFilled20 className="comparison-page--icon"/>Skills</td>
+                    <td><CheckMarkOutline20 className="comparison-page--icon"/>Skills</td>
                 </tr>
                 <tr>
                     <td><CheckMarkFilled20 className="comparison-page--icon"/>Education</td>
                     <td><CheckMarkFilled20 className="comparison-page--icon"/>Education</td>
                 </tr>
                 <tr>
-                    <td><CheckMarkFilled20 className="comparison-page--icon"/>Hobbies</td>
-                    <td><CheckMarkFilled20 className="comparison-page--icon"/>Hobbieas</td>
+                    <td><CheckMarkOutline20 className="comparison-page--icon"/>Interests</td>
+                    <td><CheckMarkFilled20 className="comparison-page--icon"/>Interests</td>
                 </tr>
                 <tr>
                     <td><CheckMarkFilled20 className="comparison-page--icon"/>Personality</td>
                     <td><CheckMarkFilled20 className="comparison-page--icon"/>Personality</td>
                 </tr>
                 <tr>
-                    <td><CheckMarkFilled20 className="comparison-page--icon"/>Languages</td>
-                    <td><CheckMarkFilled20 className="comparison-page--icon"/>Languages</td>
+                    <td><CheckMarkOutline20 className="comparison-page--icon"/>Languages</td>
+                    <td><CheckMarkOutline20 className="comparison-page--icon"/>Languages</td>
                 </tr>
                 <tr>
                     <td><Button>Pin<Pin20 className="bx--btn__icon"/></Button></td>
