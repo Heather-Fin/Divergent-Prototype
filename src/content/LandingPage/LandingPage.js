30efe4df94d848707c27f4f49d48060203574819
Heather <43322948+Heather-Fin@users.noreply.github.com>
2019-08-05 10:11:54 -0500

    landing page formatting

--- src/content/LandingPage/LandingPage.js
+++ src/content/LandingPage/LandingPage.js
@@ -13,21 +13,21 @@ class LandingPage extends Component {
       <div className="bx--grid bx--grid--full-width">
         <div className="bx--row">
             <div className="bx--col-md-4 landing-page--container">
-              <img src={RadarGraphImg} alt="CHANGE ME HEATHER!!!!!!!!" className="landing-page--radar-graph" />
+              <img src={RadarGraphImg} alt="Radar graph chart of mockup data" className="landing-page--radar-graph" />
             </div>
             <div className="bx--col-md-2 landing-page--container">
-              <h2>Your Strengths:</h2>
-              <h3><CheckMarkFilled20 />Education</h3>
-              <p>Your team has a wide variety of educational backgrounds and certifications. This means your team is well-equipped to tackle problems from many different academic perspectives.</p>
-              <h3><CheckMarkFilled20 />Languages</h3>
-              <p>Your team has a diverse set of communication styles, meaning your team has many ways of listening and sharing ideas.</p>
+              <h2 className="landing-page--h2">Your Strengths:</h2>
+              <h3 className="landing-page--h3"><CheckMarkFilled20 className="landing-pace--icons"/>Education</h3>
+              <p className="landing-page--p">Your team has a wide variety of educational backgrounds and certifications. This means your team is well-equipped to tackle problems from many different academic perspectives.</p>
+              <h3 className="landing-page--h3"><CheckMarkFilled20 className="landing-pace--icons"/>Languages</h3>
+              <p className="landing-page--p">Your team has a diverse set of communication styles, meaning your team has many ways of listening and sharing ideas.</p>
             </div>
             <div className="bx--col-md-2 landing-page--container">
-              <h2>Room to Improve:</h2>
-              <h3><ErrorFilled20 />Skills</h3>
-              <p>Your team's skills are limited mostly to technical experience. Tap into your team's potential to grow their skillset through the resources below.</p>
-              <h3><ErrorFilled20 />Culture</h3>
-              <p>Having a low score in culture means your team comes from similar backgrounds. You may have a limited perspective when tackling new projects.</p>
+              <h2 className="landing-page--h2">Room to Improve:</h2>
+              <h3 className="landing-page--h3"><ErrorFilled20 className="landing-pace--icons"/>Skills</h3>
+              <p className="landing-page--p">Your team's skills are limited mostly to technical experience. Tap into your team's potential to grow their skillset through the resources below.</p>
+              <h3 className="landing-page--h3"><ErrorFilled20 className="landing-pace--icons"/>Culture</h3>
+              <p className="landing-page--p">Having a low score in culture means your team comes from similar backgrounds. You may have a limited perspective when tackling new projects.</p>
             </div>
         </div>
       </div>
